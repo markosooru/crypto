357e87d2b72c9f09d09cee43c7723e3035d64364
markosooru <marko.sooru@gmail.com>
2024-07-25 15:07:34 +0300

    add indexes for portfolio WHERE cols

---
+++ src/main/resources/db/migration/V8__create_idx_portfolio_appuser_isdeleted.sql
@@ -0,0 +1,1 @@
+create index idx_portfolio_appUser_isDeleted on Portfolio(app_user_id, is_deleted);
---
+++ src/main/resources/db/migration/V9__create_idx_portfolio_id_appuser_isdeleted.sql
@@ -0,0 +1,1 @@
+create index idx_portfolio_id_appUser_isDeleted on Portfolio(id, app_user_id, is_deleted);
